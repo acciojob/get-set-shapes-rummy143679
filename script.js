bc90b9ab58e4ebe347741742dfad9da3073ee4ea
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-14 13:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this._width;
+	}
+
+	set width(width){
+		this._width = width;
+	}
+
+	get height(){
+		return this._height;
+	}
+
+	set width(width){
+		this._width = width;
+	}
+
+	getArea(){
+		return this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side){
+		super(side, side);
+		this._side = side;
+	}
+
+	get side(){
+		return this._side;
+	}
+
+	set side(side){
+		this._side = side;
+	}
+
+	getPerimeter(){
+		return 4 * this.side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
